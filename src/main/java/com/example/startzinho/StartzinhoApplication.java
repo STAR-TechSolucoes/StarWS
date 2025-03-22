cdf26d850641565b6d7063aecad0da4ce82297cd
diocell <dicanevarolli14@gmail.com>
2025-03-22 01:42:25 -0300

    Teste

---
+++ src/main/java/com/example/startzinho/StartzinhoApplication.java
@@ -0,0 +1,13 @@
+package com.example.startzinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartzinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartzinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/startzinho/StartzinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startzinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartzinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
